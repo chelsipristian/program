418a97f30214fdfeb932724b0b17d366ff74b17d
chelsipristian <chelsipristian4@gmail.com>
2025-04-22 18:55:54 +0700

    coomit last

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/dashboard/users';
+    protected $redirectTo = '/dashboard/student';
 
     /**
      * Create a new controller instance.
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/dashboard/users';
+    protected $redirectTo = '/dashboard/student';
 
     /**
      * Create a new controller instance.
